2d41bed8e2e3831b14c034137af9e0219e689c59
Pa Her <pa.her@gofmx.com>
2022-09-13 15:36:46 -0500

    Add recipe and recipeList structure

--- src/App.js
+++ src/App.js
@@ -1,7 +1,29 @@
 import './App.css';
+import RecipeList from './RecipeList';
 
 function App() {
-  return "hello world";
+  return (
+    <div>
+      <RecipeList recipes={sampleReceipes} />
+    </div>
+  )
 }
+
+const sampleReceipes = [
+  {
+    id: 1, 
+    name: 'Plain Chicken',
+    servings: 2,
+    cookTime: '1:45',
+    instructions: '1. Put salt on chicken\n 2. Put chicken in oven\n 3. Eat chicken'
+  },
+  {
+    id: 2, 
+    name: 'Plain Pork',
+    servings: 4,
+    cookTime: '1:45',
+    instructions: '1. Put salt on pork\n 2. Put pork in oven\n 3. Eat pork'
+  }
+]
 
 export default App;
--- src/RecipeList.js
+++ src/RecipeList.js
@@ -1,7 +1,17 @@
 import React from 'react'
+import Recipe from './Recipe'
 
-export default function RecipeList() {
+export default function RecipeList({ recipes }) {
   return (
-    <div>RecipeList</div>
+    <div>
+      {recipes.map(recipe => {
+        return (
+          <Recipe
+            key={recipe.id}
+            {...recipe}
+          />
+        )
+      })}
+    </div>
   )
 }
