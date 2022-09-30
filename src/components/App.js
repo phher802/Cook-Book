5cab218b962ff00168a6eebeaddd2ca6c01fdbd5
Pa Her <pa.her@gofmx.com>
2022-09-30 14:07:39 -0500

    Add functionality to add and delete buttons

--- src/components/App.js
+++ src/components/App.js
@@ -1,48 +1,92 @@
+import React, { useState, useEffect } from 'react';
 import '../css/App.scss';
 import RecipeList from './RecipeList';
+import { v4 as uuidv4 } from 'uuid'
+
+export const RecipeContext = React.createContext();
+const LOCAL_STORAGE_KEY = 'paCookBook.recipes';
 
 function App() {
+  const [recipes, setRecipes] = useState(sampleRecipes)
+
+  useEffect(() => {
+    const recipeJSON = localStorage.getItem(LOCAL_STORAGE_KEY)
+    if(recipeJSON != null) setRecipes(JSON.parse(recipeJSON))
+  }, [])
+
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(recipes))
+  }, [recipes])
+
+  const recipeContextValue = {
+    handleRecipeAdd,
+    handleRecipeDelete
+  }
+
+  function handleRecipeAdd() {
+    const newRecipe = {
+      id: uuidv4(),
+      name: "New",
+      servings: 1,
+      cookTime: '1:00',
+      instructions: 'Intr.',
+      ingredients: [
+        {
+          id: uuidv4(),
+          name: 'Name',
+          amount: '1 Tbsp'
+        }
+      ]
+    }
+    setRecipes([...recipes, newRecipe])
+  }
+
+  function handleRecipeDelete(id) {
+    setRecipes(recipes.filter(recipe => recipe.id !== id))
+  }
+
   return (
-    <div>
-      <RecipeList recipes={sampleReceipes} />
-    </div>
+    <RecipeContext.Provider value={recipeContextValue}>
+      <RecipeList recipes={recipes} />
+    </RecipeContext.Provider>
   )
 }
 
-const sampleReceipes = [
+
+const sampleRecipes = [
   {
-    id: 1, 
+    id: 1,
     name: 'Plain Chicken',
     servings: 2,
     cookTime: '1:45',
     instructions: '1. Put salt on chicken\n2. Put chicken in oven\n3. Eat chicken',
     ingredients: [
       {
-        id: 1, 
+        id: 1,
         name: 'Chicken',
         amount: '2 lbs'
-      }, 
+      },
       {
-        id: 2, 
+        id: 2,
         name: 'Salt',
         amount: '2 tsp'
       }
     ]
   },
   {
-    id: 2, 
+    id: 2,
     name: 'Plain Pork',
     servings: 4,
     cookTime: '1:45',
     instructions: '1. Put salt on pork\n2. Put pork in oven\n3. Eat pork',
     ingredients: [
       {
-        id: 1, 
+        id: 1,
         name: 'Pork',
         amount: '8 lbs'
-      }, 
+      },
       {
-        id: 2, 
+        id: 2,
         name: 'Paprika',
         amount: '4 tsp'
       }
